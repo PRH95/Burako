22b0d9a4fdc3be2f3000a14fdcd601f63a41da6f
PRH95 <pedro.rocha.h@hotmail.com>
2020-07-08 14:12:36 -0500

    Creacion de servidor local

--- index.js
+++ index.js
@@ -1,11 +1,19 @@
 "use strict";
 
 var mongoose = require("mongoose");
+var app = require("./app");
+var port = process.env.PORT || 3977;
 
 mongoose.connect("mongodb://localhost:27017/Burako", (err, res) => {
   if (err) {
     throw err;
   } else {
     console.log("La base de datos ha sido conectada exitosamente");
+
+    app.listen(port, function () {
+      console.log(
+        `Servidor de API rest usuarios excuchando en http://localhost: ${port}`
+      );
+    });
   }
 });

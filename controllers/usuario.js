e0ba90eeb924692228246d4f1fb06fa16696cf61
PRH95 <pedro.rocha.h@hotmail.com>
2020-07-08 15:46:25 -0500

    Controlador de usuario para mandar datos a la BD

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,10 +1,32 @@
 "user strict";
+var bcrypt = require("bcrypt-nodejs");
+var Usuario = require("../models/usuario");
 
 function pruebas(req, res) {
   res.status(200).send({
     message:
       "Probando una accion del controlador de usuarios del api rest con node y mongo",
   });
+}
+
+function saveUser(req, res) {
+  var user = new Usuario();
+
+  var params = req.body;
+
+  console.log(params);
+
+  user.nombre = params.nombre;
+  user.apellidos = params.apellidos;
+  user.correo = params.correo;
+  user.role = "ROLE_USER";
+  user.imagen = "null";
+
+  if (params.contraseña) {
+    //Encriptar contraseña y guardar datos
+  } else {
+    res.status(200).send({ message: "Introduce la contraseña" });
+  }
 }
 
 module.exports = {

48a7502e98ff5d9125f78596c4f97fde2224dd8f
PRH95 <pedro.rocha.h@hotmail.com>
2020-07-09 14:43:40 -0500

    archivos modificados

--- api/routes/user.js
+++ api/routes/user.js
@@ -2,9 +2,11 @@
 
 var express = require("express");
 var userController = require("../controllers/usuario");
+const { saveUser } = require("../controllers/usuario");
 
 var api = express.Router();
 
 api.get("/probandoControlador", userController.pruebas);
+api.get("/register", saveUser);
 
 module.exports = api;
